f2b18b3bbbf26f9c995f7e2964a552f9a25cb53c
seba <sebas.a.nunez@gmail.com>
2018-12-06 17:58:05 -0300

    agrege un login generico

--- app/src/main/java/rboock/marketapp/MainActivity.java
+++ app/src/main/java/rboock/marketapp/MainActivity.java
@@ -1,13 +1,22 @@
 package rboock.marketapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtNombre;
+    EditText edtPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtNombre = (EditText) findViewById(R.id.edtNombre);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+    }
+
+    public void Ingresar(View view){
     }
 }
