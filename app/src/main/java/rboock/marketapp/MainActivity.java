bd296d34876922299a00179d84727166c457df2c
Wes137 <sebas.a.nunez@gmail.com>
2018-12-14 00:00:56 -0300

    Agrege crear cuenta y login

--- app/src/main/java/rboock/marketapp/MainActivity.java
+++ app/src/main/java/rboock/marketapp/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView Bienvenido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String dato = getIntent().getStringExtra("dato");
+
+        if(dato.equals(null)){     // despues validare si a iniciado sesion antes y si lo ha hecho debera guardarlo
+            Intent i = new Intent(this,login.class);
+            startActivity(i);
+        }
+
+        Bienvenido = (TextView) findViewById(R.id.Bienvenido);
+        Bienvenido.setText("Bienvenido, " + dato);
+
     }
 
     public void onClickVendedor(View v){
